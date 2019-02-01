39a5b7cb0045585be14b7f7ffbebd476d4c96edd
Oscar Gustafsson <oscar-gustafsson@live.com>
2019-02-01 18:33:10 +0100

    added text in third example

--- third-example/write.js
+++ third-example/write.js
@@ -1,3 +1,4 @@
+/* Variables with bluebird module */
 var fs = require('fs');
 var Promise = require('bluebird');
 var readFile = Promise.promisify(fs.readFile);
@@ -8,6 +9,10 @@ var promiseArray =
     readFile('firstName.txt'), 
     readFile('lastName.txt')
     ];
+/* 
+Here is take the data from both of the files and loop the content inside out
+and after that I take the content, create the full name with added content.
+*/
 
 Promise.all(promiseArray).then((dataArray) => {
     var data = '';
