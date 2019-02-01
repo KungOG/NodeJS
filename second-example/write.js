321d9ee27a339710a2bb33bd7c6f02768d1e07e2
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-02-01 17:36:43 +0100

    second-example works

--- second-example/write.js
+++ second-example/write.js
@@ -1,19 +1,22 @@
 const fs = require('fs');
-
-fs.writeFile('./firstName.txt', 'Gustaf ', (err1) => {
-    fs.writeFile('./lastName.txt', 'Gustafsson', (err2) => {
-        fs.readFile('firstName.txt', 'lastName.txt', (err3, data) => {
-            if (err1 || err2 || err3) {
-                console.error(err1|| err2 || err3)
-            }
-            let combinedData = data
-            fs.writeFile('./fullName.txt', combinedData, (err) => {
-                if (err) {
-                    console.error(err)
-                }
-                else {
-                    console.log('You have sucessfully created, First, Last and FullName!')
-                }
+/* 
+First I create 2 files, with the text Gustaf and Gustafson inside.
+after that I read both of them files, and collect their data in data1/2.
+Last I add the data and created the full name. 
+*/
+fs.writeFile('./firstName.txt', 'Gustaf', (err) => {
+    fs.writeFile('./lastName.txt', 'Gustafsson', (err1) => {
+        fs.readFile('firstName.txt', (err2, data1) => {
+            fs.readFile('lastName.txt', (err3, data2) => {
+                let data = data1 + ' ' + data2;
+                fs.writeFile('./fullName.txt', data, (err4) => {
+                    if (err || err1 || err2 || err3 || err4) {
+                        console.error(err || err1 || err2 || err3 || err4)
+                    }   
+                    else {
+                        console.log('You have sucessfully created, First, Last then FullName!')
+                    }
+                })
             })
         })
     });
